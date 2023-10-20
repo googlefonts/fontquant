8e64f793a54fcd6aa89e8067cb3fce453e2743e6
Yanone <post@yanone.de>
2023-10-20 15:56:27 +0200

    Break apart tests into categories using includes selector

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,30 +6,25 @@ def get_font_path(filename):
     return os.path.dirname(os.path.realpath(__file__)) + "/fonts/" + filename
 
 
-def get_result(filename):
+def get_result(filename, includes=None, excludes=None):
     font_path = get_font_path(filename)
 
     ttFont = CustomTTFont(font_path)
     vhb = CustomHarfbuzz(font_path)
 
     base = Base(ttFont, vhb)
-    return base.value()
-
-
-farro = get_result("Farro-Regular.ttf")
-foldit = get_result("Foldit-VariableFont_wght.ttf")
-youngserif = get_result("YoungSerif-Regular.ttf")
-robotoflex = get_result("RobotoFlex-Var.ttf")
-bodonimoda = get_result("BodoniModa_18pt-Italic.ttf")
+    return base.value(includes, excludes)
 
 
 def test_numerals():
+    farro = get_result("Farro-Regular.ttf", includes=["numerals"])
     assert farro["numerals"]["proportional_oldstyle"]["value"] is False
     assert farro["numerals"]["tabular_oldstyle"]["value"] is False
     assert farro["numerals"]["proportional_lining"]["value"] is True
     assert farro["numerals"]["tabular_lining"]["value"] is True
     assert farro["numerals"]["default_numerals"]["value"] == "proportional_lining"
 
+    foldit = get_result("Foldit-VariableFont_wght.ttf", includes=["numerals"])
     assert foldit["numerals"]["proportional_oldstyle"]["value"] is False
     assert foldit["numerals"]["tabular_oldstyle"]["value"] is False
     # Foldit has tabular_lining numerals by default and an additional .lf set
@@ -42,17 +37,19 @@ def test_numerals():
 def test_appearance():
     # TODO:
     # Test for Foldit, which errors out
+    farro = get_result("Farro-Regular.ttf", includes=["appearance"])
     assert farro["appearance"]["stroke_contrast_ratio"]["value"] == 0.94
     assert farro["appearance"]["stroke_contrast_angle"]["value"] == 0
-    assert youngserif["appearance"]["stroke_contrast_ratio"]["value"] == 0.55
-    assert youngserif["appearance"]["stroke_contrast_angle"]["value"] == 25
-    assert robotoflex["appearance"]["stroke_contrast_ratio"]["value"] == 0.8
-    assert robotoflex["appearance"]["stroke_contrast_angle"]["value"] == 0
-    assert bodonimoda["appearance"]["stroke_contrast_ratio"]["value"] == 0.15
-    assert bodonimoda["appearance"]["stroke_contrast_angle"]["value"] == -18
-
     assert farro["appearance"]["weight"]["value"] == 0.295
     assert farro["appearance"]["width"]["value"] == 0.589
+
+    youngserif = get_result("YoungSerif-Regular.ttf", includes=["appearance"])
+    assert youngserif["appearance"]["stroke_contrast_ratio"]["value"] == 0.55
+    assert youngserif["appearance"]["stroke_contrast_angle"]["value"] == 25
+
+    bodonimoda = get_result("BodoniModa_18pt-Italic.ttf", includes=["appearance"])
+    assert bodonimoda["appearance"]["stroke_contrast_ratio"]["value"] == 0.15
+    assert bodonimoda["appearance"]["stroke_contrast_angle"]["value"] == -18
 
 
 def test_helpers():
