85a64753275c4b612550fd97bc7c459e8a9ea544
Yanone <post@yanone.de>
2024-04-17 15:10:09 +0200

    Moved update_documentation.py into scripts folder

--- scripts/update_documentation.py
+++ scripts/update_documentation.py
@@ -2,9 +2,9 @@ import sys
 import os
 from fontquant import Base
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/Lib")
-doc_path = os.path.dirname(os.path.realpath(__file__)) + "/Lib/fontquant/README.md"
-header_path = os.path.dirname(os.path.realpath(__file__)) + "/Lib/fontquant/README.header.md"
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../Lib")
+doc_path = os.path.dirname(os.path.realpath(__file__)) + "/../Lib/fontquant/README.md"
+header_path = os.path.dirname(os.path.realpath(__file__)) + "/../Lib/fontquant/README.header.md"
 
 
 base = Base(None, None)
