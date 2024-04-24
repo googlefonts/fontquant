d42964237079e0094ae279af9b10c9d5c35347d2
Yanone <post@yanone.de>
2024-04-24 16:22:12 +0200

    Turn cProfile into command line option

--- Lib/fontquant/cli.py
+++ Lib/fontquant/cli.py
@@ -3,12 +3,8 @@ import json
 from fontquant import CustomHarfbuzz, CustomTTFont, Base
 import argparse
 
-# import cProfile, pstats
-
 
 def cli():
-    # profiler = cProfile.Profile()
-    # profiler.enable()
 
     arg_parser = argparse.ArgumentParser(description="Measure font statistics.")
     arg_parser.add_argument("-i", action="append", help="Include metrics by their (partial) path")
@@ -22,8 +18,20 @@ def cli():
             "the origin location is measured."
         ),
     )
+    arg_parser.add_argument(
+        "-p",
+        action="store_true",
+        help=("Developer option: Profile the code. Outputs a list of functions sorted by total time. "),
+    )
     arg_parser.add_argument("font", help="Font file (.ttf or .otf)")
     options = arg_parser.parse_args(sys.argv[1:])
+
+    if options.p:
+        import cProfile
+        import pstats
+
+        profiler = cProfile.Profile()
+        profiler.enable()
 
     ttFont = CustomTTFont(sys.argv[-1])
     vhb = CustomHarfbuzz(sys.argv[-1])
@@ -32,6 +40,7 @@ def cli():
     formatted = json.dumps(base.value(options.i, options.x), indent=2)
     print(formatted)
 
-    # profiler.disable()
-    # stats = pstats.Stats(profiler).sort_stats("tottime")
-    # stats.print_stats()
+    if options.p:
+        profiler.disable()
+        stats = pstats.Stats(profiler).sort_stats("tottime")
+        stats.print_stats()
