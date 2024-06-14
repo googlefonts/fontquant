85dbd2bfed867afe50d728294de1ce5deefc7cb1
Yanone <post@yanone.de>
2024-06-14 12:02:58 +0200

    Completely removed pixel skeleton option, use kurbopy for rest

--- Lib/fontquant/helpers/stroke_contrast.py
+++ Lib/fontquant/helpers/stroke_contrast.py
@@ -8,17 +8,12 @@ import matplotlib.patches as mpatches
 import numpy as np
 import seaborn as sns
 
-from beziers.path import BezierPath
-from beziers.point import Point
-from beziers.path.representations.Nodelist import Node
-from PIL import ImageOps
-from fontquant.helpers.images import svg_to_img
+from kurbopy import Point, Vec2, Line, Vec2
 
-from fontquant import CustomTTFont
 
 # Distance of measurement points.
 # Smaller values lead to more segments and more accurate results, but is slower.
-PRECISION = 20
+PRECISION = 0.1
 
 
 def remove_outliers(array, column):
@@ -54,7 +49,11 @@ def Interpolate(a, b, p, limit=False):
         return i
 
 
-def point(point, ax):
+def interpolate_point(a, b, p):
+    return Point(Interpolate(a.x, b.x, p), Interpolate(a.y, b.y, p))
+
+
+def draw_point(point, ax):
     circle = plt.Circle((point.x, point.y), 2)
     ax.add_patch(circle)
 
@@ -65,72 +64,38 @@ def line(p1, p2, ax, color="gray", width=1):
     ax.add_patch(pp)
 
 
+def rotate(p, origin=(0, 0), degrees=0):
+    angle = np.deg2rad(degrees)
+    R = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    o = np.atleast_2d(origin)
+    p = np.atleast_2d(p)
+    return np.squeeze((R @ (p.T - o.T) + o.T).T)
+
+
+def intersections(paths, line):
+    intersections = []
+
+    for glyph_path in paths:
+        intersections.extend(glyph_path.intersections(line))
+
+    return intersections
+
+
 def stroke_contrast(paths, width, ascender, descender, show=False):
     """\
     Calculate ratio between thickest and thinnest stroke of a BezierPath
     """
 
-    # Settings
-    margin = 50
-
-    show_skeleton = True
-    show_measurements = True
-
     # Remove overlaps
-    for path in paths:
-        path.removeOverlap()
-
-    # Smooth
+    # TODO: Find a way to reactiveate this
     # for path in paths:
-    #     path.smooth(maxCollectionSize=1, lengthLimit=20)
+    #     path.removeOverlap()
 
     # Prepare matplotlib
     if show:
         f, (ax3, ax4, ax5) = plt.subplots(1, 3, figsize=(15, 15))
 
-    # Adjust path position from bounds calculations
-    x_shift = 0
-    for path in paths:
-        bounds = path.bounds()
-        if bounds.bl.x < margin:
-            x_shift = max(x_shift, margin - bounds.bl.x)
-        if bounds.bl.y < descender + margin:
-            descender = bounds.bl.y - margin
-        if bounds.tr.y > ascender - margin:
-            ascender = bounds.tr.y + margin
-        if bounds.tr.x > width - margin:
-            width = bounds.tr.x + margin
-    width += x_shift
-    for path in paths:
-        path.translate(Point(x_shift, 0))
     height = ascender - descender
-    for path in paths:
-        path.translate(Point(0, -descender))
-
-    via_skeleton = False
-
-    scale = 1.0
-    if via_skeleton:
-        from skimage.morphology import skeletonize
-        from skan import Skeleton
-
-        # Write SVG
-        svg = f'<svg viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg"><path d="'
-        for path in paths:
-            svg += path.asSVGPath()
-        svg += '" /></svg>'
-
-        img = svg_to_img(svg)
-        img = ImageOps.invert(img)
-
-        # Scale down image
-        scale = 2.5
-        width, height = img.size
-        img = img.resize((int(width / scale), int(height / scale)))
-
-        # Skeletonize
-        skeleton_lee = skeletonize(np.array(img), method="lee")
-        sk = Skeleton(skeleton_lee)
 
     # Analyze
     strokes = pandas.DataFrame(columns=["center", "p1", "p2", "thickness", "position"])
@@ -139,102 +104,75 @@ def stroke_contrast(paths, width, ascender, descender, show=False):
     if True:
         i = 0
 
-        if via_skeleton:
-            skeleton_path = BezierPath.fromPoints([Point(x, y) for y, x, _ in sk.path_coordinates(i)], error=200)
-        else:
-            # Sort paths by size
-            paths = sorted(paths, key=lambda x: x.bounds().width, reverse=True)
-            skeleton_path = paths[0]
+        # Sort paths by size
+        paths = sorted(paths, key=lambda x: x.bounding_box().width(), reverse=True)
+        skeleton_path = paths[0]
 
         # Scale up again
-        if via_skeleton:
-            skeleton_path.scale(scale)
-        # skeleton_path.tidy(maxCollectionSize=0, lengthLimit=50)
-        # lower number here leads to more segments
         skeleton_path = skeleton_path.flatten(PRECISION)
 
-        nodeList = skeleton_path.asNodelist()
-        for i, node in enumerate(nodeList):
+        for i, point in enumerate(skeleton_path):
             if i == 0:
-                j = len(nodeList) - 1
+                j = len(skeleton_path) - 1
             else:
                 j = i - 1
-            previousNode = nodeList[j]
+            previous_point = skeleton_path[j]
 
-            if node.point == previousNode.point:
+            if point.x == previous_point.x and point.y == previous_point.y:
                 continue
 
             # Find point halfway between node and previous node
-            halfway_point = Interpolate(node.point, previousNode.point, 0.5)
+            halfway_point = point.midpoint(previous_point)
 
-            distance = node.point.distanceFrom(previousNode.point)
+            distance = point.distance(previous_point)
 
-            if show and show_skeleton:
-                point(halfway_point, ax3)
+            if show:
+                draw_point(halfway_point, ax3)
 
             # Rotate node 90° and variate angle to find the shortest distance at halfway_point
             angles = pandas.DataFrame(columns=["p1", "p2", "thickness"])
 
             for angle in range(-30, 31, 3):
-                outside_point = node.point.rotated(halfway_point, math.radians(90 + angle))
 
-                # distance
-                distance = outside_point.distanceFrom(halfway_point)
+                outside_point = Point(*rotate((point.x, point.y), (halfway_point.x, halfway_point.y), 90 + angle))
+                distance = outside_point.distance(halfway_point)
 
                 if distance:
                     # Create line with endpoints far outside glyph
                     # TODO: Find better way to calculate the 5000 distance, based on font's UPM
-                    far_outside_point_1 = Interpolate(halfway_point, outside_point, 5000 / distance)
-                    far_outside_point_2 = Interpolate(halfway_point, outside_point, -1 * (5000 / distance))
-                    measurement_line_path = BezierPath.fromNodelist(
-                        [
-                            Node(far_outside_point_1.x, far_outside_point_1.y, "line"),
-                            Node(far_outside_point_2.x, far_outside_point_2.y, "line"),
-                        ]
-                    )
-                    measurement_line_segments = measurement_line_path.asSegments()
-                    # line(far_outside_point_1, far_outside_point_2, ax3)
-
-                    # Iterate over glyph segments
-                    measurement_line_segment = measurement_line_segments[0]
-                    intersections_list = []
-                    for glyph_path in paths:
-                        glyph_segments = glyph_path.asSegments()
-                        for glyph_segment in glyph_segments:
-                            intersections = measurement_line_segment.intersections(glyph_segment)
-                            intersections_list.extend(intersections)
+                    far_outside_point_1 = interpolate_point(halfway_point, outside_point, 5000 / distance)
+                    far_outside_point_2 = interpolate_point(halfway_point, outside_point, -1 * (5000 / distance))
+                    measurement_line = Line(far_outside_point_1, far_outside_point_2)
+                    intersections_list = intersections(paths, measurement_line)
 
                     # Found 2 or more intersections
-                    #  and len(intersections_list) >= 2
                     if intersections_list:
                         # Sort points by distance to node and shorten list
                         if len(intersections_list) > 2:
-                            intersections_list.sort(key=lambda x: x.point.distanceFrom(halfway_point))
+                            intersections_list.sort(key=lambda x: x.distance(halfway_point))
                             intersections_list = intersections_list[:2]
 
-                        if len(intersections_list) >= 2:
-                            angles.loc[len(angles.index)] = [
-                                intersections_list[0].point,
-                                intersections_list[1].point,
-                                intersections_list[0].point.distanceFrom(intersections_list[1].point),
-                            ]
+                        angles.loc[len(angles.index)] = [
+                            intersections_list[0],
+                            intersections_list[1],
+                            intersections_list[0].distance(intersections_list[1]),
+                        ]
 
             # Process different angles measured at halfway_point
             remove_outliers(angles, "thickness")
             min_angle = angles.loc[angles["thickness"].idxmin()]
 
-            # Add shortest line to stokes list
+            # Add shortest line to strokes list
             strokes.loc[len(strokes.index)] = [
                 halfway_point,
                 min_angle["p1"],
                 min_angle["p2"],
                 min_angle["thickness"],
-                i / len(nodeList),
+                i / len(skeleton_path),
             ]
-
-            if show and show_measurements:
-                point(min_angle["p1"], ax3)
-                point(min_angle["p2"], ax3)
+            if show:
+                draw_point(min_angle["p1"], ax3)
+                draw_point(min_angle["p2"], ax3)
                 line(min_angle["p1"], min_angle["p2"], ax3)
 
     # https://www.geeksforgeeks.org/detect-and-remove-the-outliers-using-python/
@@ -242,7 +180,8 @@ def stroke_contrast(paths, width, ascender, descender, show=False):
     if show:
         sns.boxplot(strokes["thickness"], ax=ax4)
 
-    remove_outliers(strokes, "thickness")
+    # This caused faults in some cases, removing the maximum value
+    # remove_outliers(strokes, "thickness")
 
     if show:
         sns.boxplot(strokes["thickness"], ax=ax5)
@@ -263,10 +202,10 @@ def stroke_contrast(paths, width, ascender, descender, show=False):
             break
 
     if show:
-        line(max_row["p1"], max_row["p2"], ax3, color="red", width=2)
-        line(min_row["p1"], min_row["p2"], ax3, color="blue", width=2)
+        line(max_row["p1"], max_row["p2"], ax3, color="red", width=4)
+        line(min_row["p1"], min_row["p2"], ax3, color="blue", width=4)
         if other_min is not None:
-            line(other_min["p1"], other_min["p2"], ax3, color="blue", width=2)
+            line(other_min["p1"], other_min["p2"], ax3, color="blue", width=4)
 
         ax3.set_aspect("equal")
         ax3.set_ylim(0, height)
@@ -276,10 +215,10 @@ def stroke_contrast(paths, width, ascender, descender, show=False):
     contrast_angle = None
     if other_min is not None:
         if other_min["center"].y > min_row["center"].y:
-            vector = other_min["center"] - min_row["center"]
+            vector = Vec2(min_row["center"].x - other_min["center"].x, min_row["center"].y - other_min["center"].y)
         else:
-            vector = min_row["center"] - other_min["center"]
-        contrast_angle = math.degrees(vector.angle) - 90
+            vector = Vec2(other_min["center"].x - min_row["center"].x, other_min["center"].y - min_row["center"].y)
+        contrast_angle = math.degrees(vector.atan2()) + 90
         if show:
             line(other_min["center"], min_row["center"], ax3, color="gray", width=2)
 
@@ -290,14 +229,27 @@ def stroke_contrast(paths, width, ascender, descender, show=False):
 
 
 if __name__ == "__main__":
+
+    from fontquant import CustomTTFont, CustomHarfbuzz
     import sys
+    from kurbopy import BezPathCreatingPen
 
     ttFont = CustomTTFont(sys.argv[-1])
+    vhb = CustomHarfbuzz(sys.argv[-1])
     character = ttFont.glyphname_for_char("o")
+
+    buf = vhb.shape(character)
+    pen = BezPathCreatingPen()
+    for info in buf.glyph_infos:
+        vhb._hbfont.draw_glyph_with_pen(info.codepoint, pen)
+    paths = pen.paths
+
     width = ttFont.getGlyphSet()[character].width
-    paths = BezierPath.fromFonttoolsGlyph(ttFont, character)
     descender = ttFont["hhea"].descender
     ascender = ttFont["hhea"].ascender
     assert descender <= 0
 
+    measurement_line = Line(Point(51, 411), Point(534, 454))
+
     print(stroke_contrast(paths, width, ascender, descender, show=True))
+    # print(intersections(paths, measurement_line))
